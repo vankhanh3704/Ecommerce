c9c480c242f3c86e38937660c84bbd6202a90a21
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-10-12 17:23:09 +0700

    add api introspect để kiểm tra token còn hiệu lực không

--- src/main/java/com/hikiw/ecommerce/Controller/AuthenticationController.java
+++ src/main/java/com/hikiw/ecommerce/Controller/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.hikiw.ecommerce.Controller;
 
 import com.hikiw.ecommerce.Model.Request.AuthenticationRequest;
+import com.hikiw.ecommerce.Model.Request.IntrospectRequest;
 import com.hikiw.ecommerce.Model.Response.ApiResponse;
 import com.hikiw.ecommerce.Model.Response.AuthenticationResponse;
+import com.hikiw.ecommerce.Model.Response.IntrospectResponse;
 import com.hikiw.ecommerce.Service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,6 +29,13 @@ public class AuthenticationController {
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ApiResponse.<AuthenticationResponse>builder()
                 .result(authenticationService.authenticate(request))
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(authenticationService.introspectResponse(request))
                 .build();
     }
 }
