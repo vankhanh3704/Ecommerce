f0cb343dde1c54d1a663c3c1e61fcb9d620e0131
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-10-13 10:53:08 +0700

    add feature delete token expiry time

--- src/main/java/com/hikiw/ecommerce/Repository/InvalidatedTokenRepository.java
+++ src/main/java/com/hikiw/ecommerce/Repository/InvalidatedTokenRepository.java
@@ -3,6 +3,8 @@ package com.hikiw.ecommerce.Repository;
 import com.hikiw.ecommerce.Entity.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+import java.util.Date;
 
+public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+    Integer deleteByExpiryTimeBefore(Date now);
 }
---
+++ src/main/java/com/hikiw/ecommerce/Scheduler/TokenCleanupScheduler.java
@@ -0,0 +1,30 @@
+package com.hikiw.ecommerce.Scheduler;
+
+
+import com.hikiw.ecommerce.Repository.InvalidatedTokenRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TokenCleanupScheduler {
+
+    InvalidatedTokenRepository invalidatedTokenRepository;
+
+    // Chạy mỗi ngày lúc 00:00 (nửa đêm)
+//    @Scheduled(cron = "0 0 0 * * ?")
+
+    @Scheduled(fixedRate = 5 * 60 * 1000) // dọn sau 5 phút
+    public void cleanExpiredTokens() {
+        Integer deleted = invalidatedTokenRepository.deleteByExpiryTimeBefore(new Date());
+        System.out.println("Đã xoá " + deleted + " token hết hạn.");
+    }
+}
